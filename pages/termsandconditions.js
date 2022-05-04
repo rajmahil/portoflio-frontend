67762a2bae3ff84ed5ca01dc047b354cb0475b79
Raj <webtest10111011@gmail.com>
2022-05-04 17:54:58 -0600

    update

--- pages/termsandconditions.js
+++ pages/termsandconditions.js
@@ -26,19 +26,21 @@ const termsandconditions = () => {
 
         <p>
           The following terminology applies to these Terms and Conditions,
-          Privacy Statement and Disclaimer Notice and all Agreements: "Client",
-          "You" and "Your" refers to you, the person log on this website and
-          compliant to the Company’s terms and conditions. "The Company",
-          "Ourselves", "We", "Our" and "Us", refers to our Company. "Party",
-          "Parties", or "Us", refers to both the Client and ourselves. All terms
-          refer to the offer, acceptance and consideration of payment necessary
-          to undertake the process of our assistance to the Client in the most
-          appropriate manner for the express purpose of meeting the Client’s
-          needs in respect of provision of the Company’s stated services, in
-          accordance with and subject to, prevailing law of Netherlands. Any use
-          of the above terminology or other words in the singular, plural,
-          capitalization and/or he/she or they, are taken as interchangeable and
-          therefore as referring to same.
+          Privacy Statement and Disclaimer Notice and all Agreements:
+          &quot;Client&quot;, &quot;You&quot; and &quot;Your&quot; refers to
+          you, the person log on this website and compliant to the Company’s
+          terms and conditions. &quot;The Company&quot;, &quot;Ourselves&quot;,
+          &quot;We&quot;, &quot;Our&quot; and &quot;Us&quot;, refers to our
+          Company. &quot;Party&quot;, &quot;Parties&quot;, or &quot;Us&quot;,
+          refers to both the Client and ourselves. All terms refer to the offer,
+          acceptance and consideration of payment necessary to undertake the
+          process of our assistance to the Client in the most appropriate manner
+          for the express purpose of meeting the Client&apos;s needs in respect
+          of provision of the Company&apos;s stated services, in accordance with
+          and subject to, prevailing law of Netherlands. Any use of the above
+          terminology or other words in the singular, plural, capitalization
+          and/or he/she or they, are taken as interchangeable and therefore as
+          referring to same.
         </p>
 
         <h3>
@@ -51,11 +53,11 @@ const termsandconditions = () => {
         </p>
 
         <p>
-          Most interactive websites use cookies to let us retrieve the user’s
-          details for each visit. Cookies are used by our website to enable the
-          functionality of certain areas to make it easier for people visiting
-          our website. Some of our affiliate/advertising partners may also use
-          cookies.
+          Most interactive websites use cookies to let us retrieve the
+          user&apos;s details for each visit. Cookies are used by our website to
+          enable the functionality of certain areas to make it easier for people
+          visiting our website. Some of our affiliate/advertising partners may
+          also use cookies.
         </p>
 
         <h3>
