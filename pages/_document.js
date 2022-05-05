5c14a2f3da6df663b9d9216ff31476029fb53f78
Raj <webtest10111011@gmail.com>
2022-05-04 18:00:24 -0600

    update

--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,10 @@ class MyDocument extends Document {
             href="https://floating-hollows-19339.herokuapp.com/uploads/SVG_Icon_ffb054b289.svg"
           ></link>
           <link rel="stylesheet" href="https://use.typekit.net/vha4vpu.css" />
+          <meta
+            property="og:image"
+            content="https://res.cloudinary.com/dmcsstcqf/image/upload/v1651693218/PortfolioOgImage_s0hwd6.png"
+          />
         </Head>
         <body>
           <Main />
