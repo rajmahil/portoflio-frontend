bb03af93260bc4a6af27781885a6bf85235f5994
Raj <webtest10111011@gmail.com>
2022-05-05 12:12:12 -0600

    update

--- pages/_document.js
+++ pages/_document.js
@@ -30,10 +30,29 @@ class MyDocument extends Document {
             href="https://floating-hollows-19339.herokuapp.com/uploads/SVG_Icon_ffb054b289.svg"
           ></link>
           <link rel="stylesheet" href="https://use.typekit.net/vha4vpu.css" />
+          <title>
+            Raj Mahil | Creative web designer and developer in Saskatoon, Canada
+          </title>
+          <meta
+            name="description"
+            content="Raj Mahil is a digital designer and web developer from Canada, passionate about creating memorable digital experiences. From eCommerce to mobile applications, Raj Mahil can help transform your vision into reality. Proudly operating from Saskatoon, SK, Canada."
+          ></meta>
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            property="og:title"
+            content="Raj Mahil | Creative web designer and developer in Saskatoon, Canada"
+          />
+          <meta property="og:url" content="https://www.rajmahil.ca/" />
+          <meta
+            property="og:description"
+            content="Raj Mahil is a digital designer and web developer from Canada, passionate about creating memorable digital experiences. From eCommerce to mobile applications, Raj Mahil can help transform your vision into reality. Proudly operating from Saskatoon, SK, Canada."
+          />
           <meta
             property="og:image"
             content="https://res.cloudinary.com/dmcsstcqf/image/upload/v1651693218/PortfolioOgImage_s0hwd6.png"
           />
+          <meta property="og:type" content="website" />
         </Head>
         <body>
           <Main />
